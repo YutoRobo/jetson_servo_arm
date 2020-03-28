ea769b950d7b9aa5bc1ab0c7b0154969245ff285
YutoRobo <yuto60648778@gmail.com>
2020-03-28 19:20:37 +0900

    use uvc_camera node

--- sh/hand_vr.sh
+++ sh/hand_vr.sh
@@ -1,4 +1,5 @@
 
 roslaunch rosbridge_server rosbridge_websocket.launch &
+rosrun uvc_camera uvc_camera_node _device:=/dev/video0 &
 rosrun rosserial_python serial_node.py /dev/ttyACM0 _baud:=115200 &
 rosrun jetson_servo_arm main.py 
